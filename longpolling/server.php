fb6ece87d72a7350b9f7ee1b24ad10a16dee8d0b
Gabriel Darezzo <gabrieldarezzo@yahoo.com.br>
2017-02-17 10:57:34 -0200

    Long Polling + Filtros + Artigo

---
+++ longpolling/server.php
@@ -0,0 +1,33 @@
+<?php
+// arquivo cujo conteúdo será enviado ao cliente
+$dataFileName = 'data.txt'; 
+ 
+while ( true ){
+    $requestedTimestamp = isset( $_GET['timestamp'] ) ? (int)$_GET['timestamp'] : null;      
+ 
+    // o PHP faz cache de operações "stat" do filesystem. Por isso, devemos limpar esse cache   
+    clearstatcache();  
+ 
+    $modifiedAt = filemtime( $dataFileName );       
+ 
+    if ( $requestedTimestamp == null || $modifiedAt > $requestedTimestamp )
+    {
+        $data = file_get_contents( $dataFileName );
+ 
+        $arrData = array(
+            'content' => $data,
+            'timestamp' => $modifiedAt
+        );
+ 
+        $json = json_encode( $arrData );
+ 
+        echo $json;
+ 
+        break;
+    }
+    else
+    {
+        sleep( 2 );
+        continue;
+    }
+}
